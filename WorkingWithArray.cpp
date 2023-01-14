02ebd77fd8327c70d7518454aa510dcfb518f34e
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-01-14 18:46:47 +0300

    Добавить элементы в массив

--- WorkingWithArray.cpp
+++ WorkingWithArray.cpp
@@ -4,5 +4,32 @@
 
 int main()
 {
+	//task #1
+	int arr[7];
 
+	printf("Input 7 elements\n");
+
+	for (int i = 0; i < 7; i++) {
+		scanf_s("%i", &arr[i]);
+	}
+
+	for (int i = 0; i < 7; i++) {
+		printf("%2i", arr[i]);
+	}
+
+	//task #2
+	int arr2[100], n = 0;
+
+	printf("\nInput array length in range of 100\n");
+	scanf_s("%i", &n);
+	printf("Input elements\n");
+
+
+	for (int i = 0; i < n; i++) {
+		scanf_s("%i", &arr2[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%2i", arr2[i]);
+	}
 }
