d54cbe24018c38b6c2ef80ef5d004f7660ed3174
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-01-16 12:33:30 +0300

    Реализовать сортировку "пузырьком"

--- WorkingWithArray.cpp
+++ WorkingWithArray.cpp
@@ -4,5 +4,38 @@
 
 int main()
 {
+	int n = 0, step = 0, arr[100]{};
+	bool flag = true;
+	
+	printf("Input array length ");
+	scanf_s("%i", &n);
 
+	for (int i = 0; i < n; i++) {
+		scanf_s("%i", &arr[i]);
+	}
+	for (int j = 0; j < n; j++) {
+		printf("%i ", arr[j]);
+	}
+	//sorting
+	while (flag) {
+		flag = false;
+		for (int j = 0; j < n; j++) {
+			for (int i = 0; i < n - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					int z = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = z;
+					flag = true;
+				}
+			}
+
+		}
+		step++;
+	}	
+
+	printf("\nItterations: %i\n", step);
+	printf("\n Result: ");
+	for (int i = 0; i < n; i++) {
+		printf("%i ", arr[i]);
+	}
 }
