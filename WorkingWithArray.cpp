38db5f5548298e70fc979afb28a8b9e8aca65cd1
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2023-01-19 10:07:39 +0300

    Update WorkingWithArray.cpp

--- WorkingWithArray.cpp
+++ WorkingWithArray.cpp
@@ -4,5 +4,32 @@
 
 int main()
 {
+	int arr[100] = {}, max = 0, min = 0, sum = 0, n = 0;
+
+	printf("Input elements length ");
+	scanf_s("%i", &n);
+
+	printf("Input array elements\n");
+
+	for (int i = 0; i < n; i++) {
+		scanf_s("%i", &arr[i]);
+	}
+
+	min = max = arr[0];
+
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+
+	printf("Min: %i Max: %i\n", min, max);
+	printf("Sum: %i\n", sum);
+	printf("Middle Arithmetic %f", float(sum) / float(n));
 
 }
